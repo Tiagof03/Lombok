0ec372576c3ea088b1d325d1943ac2560e933757
Tiagof03 <tiagohfunes03@gmail.com>
2025-09-05 09:46:06 -0300

    Initial commit

---
+++ src/Actividad_1/Persona.java
@@ -0,0 +1,15 @@
+package Actividad_1;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+public class Persona {
+    private String nombre;
+    private int edad;
+
+}
---
+++ src/Actividad_1/main.java
@@ -0,0 +1,13 @@
+package Actividad_1;
+
+public class main {
+    public static void main(String[] args) {
+        Persona persona = new Persona("tiago", 21);
+        Persona persona2 = new Persona("Sergio", 22);
+        Persona persona3 = new Persona("Maria", 23);
+
+        System.out.println(persona);
+        System.out.println(persona2);
+        System.out.println(persona3);
+    }
+}
